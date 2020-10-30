3744e7ece530eaee3f5025638c916f8ac18846bd
Ian Salazar <iansalazar67@gmail.com>
2020-10-30 15:47:35 +0800

    Added essential methods for Player class

--- src/user/Player.java
+++ src/user/Player.java
@@ -2,7 +2,27 @@ package user;
 
 public class Player {
 
-	private int tokenOnHand = 4;
+	private int tokenOnHand;
+
+	Player(){
+		this.tokenOnHand = 4;
+	}
+
+	void viewState(){
+		System.out.println("Player has " + this.tokenOnHand + " tokens .");
+	}
+
+	void loseToken(){
+		this.tokenOnHand--;
+	}
+
+	void gainToken(int token){
+		this.tokenOnHand += token;
+	}
+
+	int getToken(){
+		return this.tokenOnHand;
+	}
 
 	// implement methods to control tokens by player
 }
